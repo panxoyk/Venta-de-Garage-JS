19f43eca7b459bec076f34c9c6283da78ce4a2a5
Francisco Castillo <shev>
2022-08-23 19:07:34 -0400

    css, html y js del menu mobile finaliados. NavBar completamente lista

--- main.js
+++ main.js
@@ -2,9 +2,16 @@
 
 const navbarEmail = document.querySelector('.navbar__email');
 const menuDesktop = document.querySelector('.menu-desktop');
+const navbarIconMenu = document.querySelector('.navbar__icon-menu')
+const menuMobile = document.querySelector('.menu-mobile');
 
 navbarEmail.addEventListener('click',toggleMenuDesktop);
+navbarIconMenu.addEventListener('click',toggleMenuMobile);
 
 function toggleMenuDesktop() {
     menuDesktop.classList.toggle('inactive');
-}
+};
+
+function toggleMenuMobile() {
+    menuMobile.classList.toggle('inactive');
+};
