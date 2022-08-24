49c9f2d14b4b51951d0862f99693084af36fc0ce
Francisco Castillo <shev>
2022-08-23 22:14:19 -0400

    css, html y js del carrito de compras finalizado + funciones que cierran los menus al abrir el carrito y viceversa

--- main.js
+++ main.js
@@ -4,14 +4,41 @@ const navbarEmail = document.querySelector('.navbar__email');
 const menuDesktop = document.querySelector('.menu-desktop');
 const navbarIconMenu = document.querySelector('.navbar__icon-menu')
 const menuMobile = document.querySelector('.menu-mobile');
+const navbarCart = document.querySelector('.navbar__cart');
+const asideCart = document.querySelector('.cart');
 
 navbarEmail.addEventListener('click',toggleMenuDesktop);
 navbarIconMenu.addEventListener('click',toggleMenuMobile);
+navbarCart.addEventListener('click',toggleShoppingCart);
 
 function toggleMenuDesktop() {
+    const asideCartClose = asideCart.classList.contains('inactive');
+
+    if (!asideCartClose) {
+        asideCart.classList.add('inactive');
+    };
+
     menuDesktop.classList.toggle('inactive');
 };
 
 function toggleMenuMobile() {
+    const asideCartClose = asideCart.classList.contains('inactive');
+
+    if (!asideCartClose) {
+        asideCart.classList.add('inactive');
+    };
+
     menuMobile.classList.toggle('inactive');
+};
+
+function toggleShoppingCart() {
+    const menuMobileClose = menuMobile.classList.contains('inactive');
+    const menuDesktopClose = menuDesktop.classList.contains('inactive');
+
+    if (!menuMobileClose || !menuDesktopClose) {
+        menuMobile.classList.add('inactive');
+        menuDesktop.classList.add('inactive');
+    };
+
+    asideCart.classList.toggle('inactive');
 };
