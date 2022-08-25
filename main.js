44d83b44d1a9905d480fab6fcecedb431a32033e
Francisco Castillo <shev>
2022-08-24 22:06:40 -0400

    css y js de los productos finalizados + detalles del menu y carrito en mobile

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const navbarIconMenu = document.querySelector('.navbar__icon-menu')
 const menuMobile = document.querySelector('.menu-mobile');
 const navbarCart = document.querySelector('.navbar__cart');
 const asideCart = document.querySelector('.cart');
+const sectionProducts = document.querySelector('.products-container');
 
 navbarEmail.addEventListener('click',toggleMenuDesktop);
 navbarIconMenu.addEventListener('click',toggleMenuMobile);
@@ -42,3 +43,62 @@ function toggleShoppingCart() {
 
     asideCart.classList.toggle('inactive');
 };
+
+function renderProducts (array) {
+    for (const product of array) {
+        const productHTML = document.createElement('DIV');
+        productHTML.classList.add('product');
+
+        const productImage = document.createElement('IMG');
+        productImage.setAttribute('src',product.image);
+        productImage.setAttribute('alt',`image ${product.name}`);
+
+        const productCard = document.createElement('DIV');
+        productCard.classList.add('product__card');
+
+        const productDiv = document.createElement('DIV');
+
+        const productPrice = document.createElement('P');
+        productPrice.innerText = `$${product.price},00`;
+
+        const productName = document.createElement('P');
+        productName.innerText = product.name;
+
+        const productFigure = document.createElement('FIGURE');
+
+        const productIcon = document.createElement('IMG');
+        productIcon.setAttribute('src','./icons/bt_add_to_cart.svg');
+        productIcon.setAttribute('alt','icon add to cart');
+
+        productFigure.appendChild(productIcon);
+        productDiv.appendChild(productPrice);
+        productDiv.appendChild(productName);
+
+        productCard.appendChild(productDiv);
+        productCard.appendChild(productFigure);
+
+        productHTML.appendChild(productImage);
+        productHTML.appendChild(productCard);
+
+        sectionProducts.appendChild(productHTML);
+    };
+};
+
+const productList = [];
+for (let i = 0;i < 10;i++) {
+    productList.push({
+        name: 'Mountain bike',
+        price: 120,
+        image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    });
+
+    productList.push({
+        name: 'Sofa',
+        price: 80,
+        image: 'https://images.pexels.com/photos/1866149/pexels-photo-1866149.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    });
+}
+
+renderProducts(productList);
+
+
